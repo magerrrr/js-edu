ea5903184fdb51b4010908197f9e8c98d6fef7a0
Kirill Mager <kiruha24@mail.ru>
2019-10-09 02:24:00 +0300

    feat: passed 25 tests from 25

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(focus = 'family', knowsProgramming = true, config = {family: 4}) {
+    let countOfHoursPerWeek = config[focus];
+    getAmountOfHoursFromScratch(knowsProgramming);
+    getAmountOfWeeksToComleteTheCourse(countOfHours, countOfHoursPerWeek);
+    return amount;
+    
+    function getAmountOfHoursFromScratch(statusOfKnowProgramming){
+        if(statusOfKnowProgramming){
+            return countOfHours = 800;
+        }
+            return countOfHours = 1300;
+    }
+
+    function getAmountOfWeeksToComleteTheCourse(spendTime, spendTimePerWeek){
+        if(spendTime % spendTimePerWeek == 0){
+            amount = spendTime / spendTimePerWeek;
+        } else {
+            amount = Math.trunc(spendTime / spendTimePerWeek);
+            amount++;
+        }
+    }
+};
